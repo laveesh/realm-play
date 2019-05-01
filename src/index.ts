32e6e31cc4ba2d1959c9447209377ab2c7057150
Laveesh Gupta <loveeshgoyal@gmail.com>
2019-05-01 17:55:56 +0530

    pos operations added

--- src/index.ts
+++ src/index.ts
@@ -57,16 +57,45 @@ Realm.open({ schema: [CarSchema, PersonSchema] })
   });
 
 app.get('/', (req, res) => {
-  res.send('Connected!')
+  res.send('Connected!');
 });
 
 app.get('/car-data', (req, res) => {
-  Realm.open({ schema: [CarSchema] }).then(realm => {
-    res.status(200).json(realm.objects('Car'))
-  }).catch(err => res.status(500).json({
-    message: err ? err.message : 'error',
-    error: true
-  }))
-})
+  Realm.open({ schema: [CarSchema] })
+    .then(realm => {
+      res.status(200).json(realm.objects('Car'));
+    })
+    .catch(err =>
+      res.status(500).json({
+        message: err ? err.message : 'error',
+        error: true
+      })
+    );
+});
+app.post('/hi', (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
+});
+app.post('/add-car', (req, res) => {
+  console.log(req.body);
+  const carObj = req.body;
+  Realm.open({ schema: [CarSchema] })
+    .then(realm => {
+      realm.write(() => {
+        realm
+          .create('Car', carObj)
+          .then(carData => {
+            res.status(200).json(carData);
+          })
+          .catch(err => {
+            res.status(500).json({
+              message: err ? err.message : 'failed to add the car data',
+              error: true
+            });
+          });
+      });
+    })
+    .catch(err => res.status(500).send(err));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
